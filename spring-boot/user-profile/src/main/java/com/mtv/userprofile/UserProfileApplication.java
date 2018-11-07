9dacf988a6795e1a317d09558dcc0fe5cd3ff9ca
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2018-11-07 09:23:26 -0500

    added log level

--- spring-boot/user-profile/src/main/java/com/mtv/userprofile/UserProfileApplication.java
+++ spring-boot/user-profile/src/main/java/com/mtv/userprofile/UserProfileApplication.java
@@ -1,12 +1,22 @@
 package com.mtv.userprofile;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserProfileApplication {
 
+	private static Logger logger = LogManager.getLogger(UserProfileApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(UserProfileApplication.class, args);
+
+		logger.debug("Debugging log  - Application started");
+		logger.info("Info log  - Application started");
+		logger.warn("Warning log  - Application started");
+		logger.error("Error log  - Application started");
+		logger.fatal("Fatal log  - Application started");
 	}
 }
