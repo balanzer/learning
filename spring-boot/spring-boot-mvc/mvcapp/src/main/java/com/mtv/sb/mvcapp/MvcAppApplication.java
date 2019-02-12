e6064fdf937a3ac2f0b9e230d515376799db01d3
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2019-02-12 07:31:31 -0500

    added log4j support;

--- spring-boot/spring-boot-mvc/mvcapp/src/main/java/com/mtv/sb/mvcapp/MvcAppApplication.java
+++ spring-boot/spring-boot-mvc/mvcapp/src/main/java/com/mtv/sb/mvcapp/MvcAppApplication.java
@@ -1,13 +1,26 @@
 package com.mtv.sb.mvcapp;
 
+import java.util.Calendar;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MvcAppApplication {
 
+	private static final Logger logger = LogManager.getLogger(MvcAppApplication.class);
+
 	public static void main(String[] args) {
+
+		final String appStartTime = Calendar.getInstance().getTime().toString();
+		final String appName = "Demo MVC App";
+
 		SpringApplication.run(MvcAppApplication.class, args);
+
+		logger.info("App : {}, Started on : {} ", appName, appStartTime);
+
 	}
 
 }
