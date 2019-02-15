15ac2d57afad858ce5d3c64b0430006330a26d28
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2019-02-15 11:09:53 -0500

    listed users

--- spring-boot/spring-boot-mvc/mvcapp/src/main/java/com/mtv/sb/mvcapp/controller/ManageUserController.java
+++ spring-boot/spring-boot-mvc/mvcapp/src/main/java/com/mtv/sb/mvcapp/controller/ManageUserController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.mtv.sb.domain.User;
 import com.mtv.sb.service.ManageUsers;
@@ -21,18 +22,21 @@ public class ManageUserController {
 	ManageUsers service;
 
 	@GetMapping(path = "/listusers")
-	public String listAllusers() {
+	public ModelAndView listAllusers() {
 		logger.debug("listAllusers - start");
 		// add implementation later
 		final List<User> users = this.service.listUsers();
 
+		final ModelAndView mav = new ModelAndView("listUsers");
+
 		if (CollectionUtils.isNotEmpty(users)) {
 			logger.debug("listAllusers total users : {}", users.size());
+			mav.addObject("users", users);
 		} else {
 			logger.error("listAllusers failed to get users from service");
 		}
 
-		return "listUsers";
+		return mav;
 
 	}
 
